96778f0c039cfcbc6907ea02ad97ceb33ba5ad39
Anudeep Dugyala <121557455+agap-0251@users.noreply.github.com>
2023-07-04 11:52:32 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server : {
-    proxy: {
-      '/api' :{
-        target : "http://exp-api-qlei.onrender.com",
-   secure : false,
-      changeOrigin : true}
-      }
-
-  },
+  
   plugins: [react()]
 })
