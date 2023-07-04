ee9234697818aa0c15557bf6c9b0d49a617ea29a
Anudeep Dugyala <121557455+agap-0251@users.noreply.github.com>
2023-07-04 12:58:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-const cors = require('cors');
 
 
 const port = process.env.PORT || 3500
@@ -15,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 //middleware
-app.use(cors());
+
 app.use((req,res,next) => {
     console.log(req.method,req.path)
     next()
