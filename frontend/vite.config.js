49ad5d82b80b5da52fb3329ace724d3fe0c91116
Anudeep Dugyala <121557455+agap-0251@users.noreply.github.com>
2023-07-04 11:34:33 +0530

    Update vite.config.js again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server : {
     proxy: {
       '/api' :{
-        target : "http://exp-api-qlei.onrender.com/api",
+        target : "http://exp-api-qlei.onrender.com",
    secure : false}
       }
 
