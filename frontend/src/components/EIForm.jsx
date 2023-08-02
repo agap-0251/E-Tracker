21a7eb0b565de3400d1042898ce52f3a89653cf6
Anudeep Dugyala <121557455+agap-0251@users.noreply.github.com>
2023-08-03 00:30:17 +0530

    Update EIForm.jsx

--- frontend/src/components/EIForm.jsx
+++ frontend/src/components/EIForm.jsx
@@ -23,7 +23,7 @@ const EIForm = ({isIn}) => {
         setIsLoading(true)
         const expense = {title,amount,payDate,isIncome,category}
         // console.log(expense)
-        const res = await fetch('https://exp-backend.onrender.com/api/expenses',{
+        const res = await fetch('https://exp-backend.onrender.com/api/expenses/hello',{
             method : 'POST',
             body : JSON.stringify(expense),
             headers : {
