8e422973f33cc2f0f1944cdc9db6c09cd4d12911
Anudeep Dugyala <121557455+agap-0251@users.noreply.github.com>
2023-09-23 11:08:04 +0530

    Update SideBar.jsx

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -98,3 +98,16 @@ const SideBar = () => {
 }
 
 export default SideBar;
+
+function convertToBase64(file) {
+  return new Promise((resolve,reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      resolve(fileReader.result)
+    };
+    fileReader.onerror = (error) => {
+      reject(error)
+    }
+  })
+}
